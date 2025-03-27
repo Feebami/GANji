d509c8353890502135cb879f9ef8e2374e64639d
Feebami <chandonrhamel@gmail.com>
2025-03-27 10:45:30 +0900

    Made input data sample reverse black and white

--- make_grid.py
+++ make_grid.py
@@ -10,7 +10,7 @@ sample = np.random.choice(files, 9)
 sample = [PIL.Image.open(f'kanji/{s}') for s in sample]
 sample = [T.ToTensor()(s) for s in sample]
 sample = torch.stack(sample)
-grid = make_grid(sample, nrow=3) * 255
+grid = 255 - make_grid(sample, nrow=3) * 255
 grid = grid.permute(1, 2, 0).numpy().astype('uint8')
 grid = PIL.Image.fromarray(grid)
 grid.save('display_imgs/input_kanji_sample.png')
